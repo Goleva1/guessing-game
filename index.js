95d8e0893bad351aa0fb605372b7df9709260101
github-Goleva1 <golevaviktoria1@gmai.com>
2024-09-05 18:39:18 +0200

    fead:solution

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const GuessingGame = require('./src/guessing-game.js');
+class GuessingGame {
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.currentGuess = null;
+    }
 
-window.game = new GuessingGame();
+
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    guess() {
+        if (this.min === null || this.max === null) {
+            throw new Error('Range not set.');
+        }
+        this.currentGuess = Math.floor((this.min + this.max) / 2);
+        return this.currentGuess;
+    }
+
+    lower() {
+        if (this.currentGuess === null) {
+            throw new Error('No guess to adjust.');
+        }
+        this.max = this.currentGuess - 1;
+    }
+
+    greater() {
+        if (this.currentGuess === null) {
+            throw new Error('No guess to adjust.');
+        }
+        this.min = this.currentGuess + 1;
+    }
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = GuessingGame;
+}
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.mid = 0;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
+      this.mid = Math.round((min + max) / 2);
     }
-
+  
     guess() {
-
+      this.mid = Math.round((this.min + this.max) / 2);
+      return this.mid;
     }
-
+  
     lower() {
-
+      this.max = this.mid;
     }
-
+  
     greater() {
-
+      this.min = this.mid;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
